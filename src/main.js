bcccf5c2b2c948001fab516cf59ca75a78bb6c62
Nkwe <nkabo074@gmail.com>
2024-03-27 13:22:16 +0100

    activation of the PiP by pulstion of the start button

--- src/main.js
+++ src/main.js
@@ -5,19 +5,28 @@ const BUTTON = document.getElementById("button");
  * prompt the user to select a media stream, pass to video alement to play
  */
 async function selectMediaStream() {
-  if (confirm("allow screen capture")) {
-    try {
-      const MEDIA_STREAM = await navigator.mediaDevices.getDisplayMedia();
-      VIDEO_ELEMENT.srcObject = MEDIA_STREAM;
-      VIDEO_ELEMENT.onloadedmetadata = () => {
-        VIDEO_ELEMENT.play();
-      };
-    } catch (error) {
-      console.log(`oops something hapend ${error}`);
-      // error handling here ...
-    }
-  } 
+  try {
+    const MEDIA_STREAM = await navigator.mediaDevices.getDisplayMedia();
+    VIDEO_ELEMENT.srcObject = MEDIA_STREAM;
+    VIDEO_ELEMENT.onloadedmetadata = () => {
+      VIDEO_ELEMENT.play();
+    };
+  } catch (error) {
+    console.log(`oops something hapend ${error}`);
+    // error handling here ...
+  }
 }
+
+BUTTON.addEventListener("click", async () => {
+  // Disable the button
+  BUTTON.disabled = true;
+
+  // Start Picture in picture
+  await VIDEO_ELEMENT.requestPictureInPicture();
+
+  // reset the button
+  BUTTON.disabled = false;
+});
 
 // on load
 selectMediaStream();
